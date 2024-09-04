3497d852f550e7387e248efb1cf75a62f3bec20b
Dmitry Pereboev <dm.pereboev@gmail.com>
2024-09-04 16:54:37 +0300

    Reconsider the aims

---
+++ main.go
@@ -0,0 +1,10 @@
+package main
+
+import (
+	"fmt"
+	"waterflea/tools"
+)
+
+func main() {
+	fmt.Println(tools.Waterflea())
+}
---
+++ tools/waterflea.go
@@ -0,0 +1,5 @@
+package tools
+
+func Waterflea() string {
+	return "WATERFLEA"
+}
